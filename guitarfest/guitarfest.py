a6b0eccecf82665bd27edf60e1e2afadee8d313d
Dominique Danco <dominique@ans.app>
2024-05-14 21:52:29 +0200

    Rework

--- guitarfest/guitarfest.py
+++ guitarfest/guitarfest.py
@@ -1,63 +1,70 @@
+import csv
+
 from dataclasses import dataclass
 from functools import reduce
+from os import path
 from typing import Dict, FrozenSet, Tuple
 
 
-RANKING_LENGTH = 6
-
 Person = str
 Guitar = str
-
+Ranking = Tuple[Guitar, ...]
+Rankings = Dict[Person, Ranking]
+Ordering = Tuple[Person, ...]
 
 @dataclass(frozen=True)
 class Choice:
   person: Person
   guitar: Guitar
 
-
-Ranking = Tuple[Guitar, ...]
-Rankings = Dict[Person, Ranking]
-Ordering = Tuple[Person, ...]
-Choices = Tuple[Choice, ...]
-Choicess = Tuple[Choices, ...]
+# For example, all the first-choice picks for all participants
+ChoiceLevel = Tuple[Choice, ...]
+Choices = Tuple[ChoiceLevel, ...]
 Allocation = Dict[Person, Guitar]
 
 
-def remove_guitar_and_person(
-    all_choices: Choicess, guitar: Guitar, person: Person) -> Choicess:
-  choice_filter = lambda c: c.person != person and c.guitar != guitar
-  return tuple(tuple(filter(choice_filter, choices)) for choices in all_choices)
+def get_rankings_from_file(filepath: str) -> Rankings:
+  csvreader = csv.reader(open(path.expanduser(filepath)))
+  next(csvreader) # Skip header
+  return {row[0]: tuple(filter(lambda i: i != '', row[1:])) for row in csvreader}
 
 
-def order_choices(choicess: Choicess, ordering: Ordering) -> Choicess:
-  return tuple(tuple(sorted(choices, key=lambda c: ordering.index(c.person)))
-      for choices in choicess)
+def order_choices(choices: Choices, ordering: Ordering) -> Choices:
+  return tuple(tuple(sorted(choice_level, key=lambda c: ordering.index(c.person)))
+      for choice_level in choices)
 
 
 def build_choices_from_rankings(
-    rankings: Rankings, ordering: Ordering) -> Choicess:
+    rankings: Rankings, ordering: Ordering) -> Choices:
 
-  def get_ith_choices(i: int) -> Choices:
+  def get_ith_choice_level(i: int) -> ChoiceLevel:
     return tuple(Choice(person, ranking[i])
         for person, ranking in rankings.items() if len(ranking) > i)
 
-  return tuple(get_ith_choices(i)
-      for i in range(max(len(ranking) for ranking in rankings.values())))
+  return order_choices(tuple(get_ith_choice_level(i)
+      for i in range(max(len(ranking) for ranking in rankings.values()))), ordering)
 
 
-def allocate_guitars(choices: Choicess,
+def remove_guitar_and_person(
+    choices: Choices, guitar: Guitar, person: Person) -> Choices:
+  choice_filter = lambda c: c.person != person and c.guitar != guitar
+  return tuple(tuple(filter(choice_filter, choice_level)) for choice_level in choices)
+
+
+def allocate_guitars(choices: Choices,
                       allocation: Allocation) -> Allocation:
 
   # All possible guitars allocated
   if len(choices) == 0:
     return allocation
 
-  # No more valid allocations left at current top choice level
+  # No more valid allocations left at current choice level
   if len(choices[0]) == 0:
     return allocate_guitars(choices[1:], allocation)
 
   selection = choices[0][0]
   allocation[selection.person] = selection.guitar
+
   updated_choices = remove_guitar_and_person(
       choices, selection.guitar, selection.person)
 
@@ -66,13 +73,12 @@ def allocate_guitars(choices: Choicess,
 
 def guitar_fest(rankings: Rankings, ordering: Ordering) -> Tuple[Allocation, Allocation]:
 
-  choicess = build_choices_from_rankings(rankings, ordering)
-  first_allocation = allocate_guitars(choicess, {})
+  choices = build_choices_from_rankings(rankings, ordering)
+  first_allocation = allocate_guitars(choices, {})
 
-  def reduce_helper(acc: Choicess, pair: Tuple[Person, Guitar]) -> Choicess:
-    return remove_guitar_and_person(choicess, pair[1], pair[0])
-
-  updated_choices: Choicess = reduce(reduce_helper, first_allocation.items(), choicess)
+  allocated_pairs = tuple(Choice(person, guitar) for person, guitar in first_allocation.items())
+  updated_choices = tuple(tuple(filter(lambda c: c not in allocated_pairs, choice_level))
+      for choice_level in choices)
 
   def choice_level_in_first_round(person: Person) -> int: ## Todo: combine this with ordering function
     if person not in first_allocation:
@@ -89,24 +95,25 @@ def guitar_fest(rankings: Rankings, ordering: Ordering) -> Tuple[Allocation, All
   return (first_allocation, second_allocation)
 
 
+# ##### EXAMPLE #####
 
-##### EXAMPLE #####
+# ordering = ('ty', 'helene', 'alex', 'dominique')
 
-ordering = ('ty', 'helene', 'alex', 'dominique')
+# rankings: Rankings = {
+#   'ty': ('guitar_1', 'guitar_2', 'guitar_3'),
+#   'helene': ('guitar_1', 'guitar_2', 'guitar_4'),
+#   'alex': ('guitar_2', 'guitar_3', 'guitar_4'),
+#   'dominique': ('guitar_4', 'guitar_1', 'guitar_2')}
 
-rankings: Rankings = {'ty': ('guitar_1', 'guitar_2', 'guitar_3'),
-              'helene': ('guitar_1', 'guitar_2', 'guitar_4'),
-              'alex': ('guitar_2', 'guitar_3', 'guitar_4'),
-              'dominique': ('guitar_4', 'guitar_1', 'guitar_2')}
+# # Expected result: Ty:1, Alex:2, Dominique:4, Helene:X
 
-# Expected result: Ty:1, Alex:2, Dominique:4, Helene:X
-
-
-##################
-
+# ##################
 
 
 if __name__ == '__main__':
+
+  rankings = get_rankings_from_file('~/Downloads/rankings.csv')
+  ordering = ('P1', 'P2', 'P3', 'P4', 'P5')
 
   allocation_1, allocation_2 = guitar_fest(rankings, ordering)
 
@@ -117,6 +124,3 @@ if __name__ == '__main__':
   print('\nAllocation 2:')
   for (person, guitar) in allocation_2.items():
     print(f'{person}: {guitar}')
-
-
-
